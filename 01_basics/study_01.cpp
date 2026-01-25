933383f9e97163988551303fb19e7930b520d7a0
juhee77 <bora2233@naver.com>
2026-01-25 18:05:26 +0900

    c++ : 간접 포함

--- 01_basics/study_01.cpp
+++ 01_basics/study_01.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <string> // 간점 포함
 
 int main() {
-    std::cout << "Study 01: Hello from Basic!" << std::endl;
+    std::string name;
+    int javaYears;
+    int csharpYears;
+
+    std::cout << "개발자 닉네임을 입력하세요 : ";
+    std::cin >> name;
+    std::cout << "Java 경력(개월)을 입력하세요 : ";
+    std::cin >> javaYears;
+    std::cout << "C# 경력을(개월) 입력하세요 : ";
+    std::cin >> csharpYears;
+
+    double totalYears = javaYears / 12 + csharpYears / 12;
+    std::cout << "--------------------------------" << std::endl;
+    std::cout << "[" << name << "] 님의 총 개발 경력은 약 "
+            << totalYears << "년 입니다." << std::endl;
+    std::cout << "이제 C++ 마스터를 향해 출발합시다!" << std::endl;
     return 0;
 }
